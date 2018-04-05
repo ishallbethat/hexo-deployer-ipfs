0385e0c55323a09f43375ebf3a3f5c1a60168401
Gabriel Wu <yingpingwdz@163.com>
2018-04-05 23:17:54 +0800

    update

--- readme.md
+++ readme.md
@@ -1,16 +1,17 @@
 # hexo-deployer-ipfs
 ## this is extension for hexo to deploy static sites to ipfs network.
 
-1. Installation
-Using npm
-> cd <your hexo project>
-> npm i --save hexo-deployer-ipfs
+### Installation
+#### Using npm
+> cd 'your hexo project' <br>
+> npm i --save hexo-deployer-ipfs <br>
 
-2. Configuration
-in your _config.yml file, configure a deployer as below.
+### Configuration
+#### in your _config.yml file, configure a deployer as below.
 
   >deploy: <br>
- \- path: &ensp;"your folder, by default it's public, type string" <br>
+ \- ipfs: &ensp;"required, value is 'ipfs', type string" <br>
+    &ensp;path: &ensp;"your folder, by default it's public, type string" <br>
     &ensp;host: &ensp;"your ipfs node hostname, by default localhost, type string" <br>
     &ensp;port: &ensp;"your ipfs port, by default 5001, type integer" <br>
     &ensp;protocol: &ensp;"protocol, by default http, type string" <br>
--- test.js
+++ test.js
@@ -1,21 +1,36 @@
 var deployer = require('./lib/deployer.js');
-// var data={
-//     path: 'folder',
-//     port: 5001,
-//     host: 'localhost',
-//     protocol: 'http'
-// };
-// deployer.addDir(data,function(err,result){
-//   if(!err){
-//     console.log(result);
-//   }else{
-//     console.error(err);
-//   }
-// });
-deployer.addDir(null,function(err,result){
+var data={
+    path: 'folder',
+    port: 5001,
+    host: 'localhost',
+    protocol: 'http',
+    type: 'ipfs'
+};
+deployer.addDir(data,function(err,result){
   if(!err){
     console.log(result);
   }else{
     console.error(err);
   }
 });
+// deployer.addDir({type: "ipfs"},function(err,result){
+//   if(!err){
+//     console.log(result);
+//   }else{
+//     console.error(err);
+//   }
+// });
+// deployer.addDir(null,function(err,result){
+//   if(!err){
+//     console.log(result);
+//   }else{
+//     console.error(err);
+//   }
+// });
+// deployer.addDir({},function(err,result){
+//   if(!err){
+//     console.log(result);
+//   }else{
+//     console.error(err);
+//   }
+// });
